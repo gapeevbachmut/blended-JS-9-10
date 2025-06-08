065c0dc82e33a9a72bd91b572c30d83ddfb56fbf
Андрій <gapeev1979@gmail.com>
2025-06-08 17:15:32 +0300

    almost done

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@
   Список із завданнями має бути доступним після перезавантаження сторінки.
 
   Розмітка картки задачі
-  <li class="task-list-item">
+  <li class="task-list-item" data-id>
       <button class="task-list-item-btn">Delete</button>
       <h3>Заголовок</h3>
       <p>Текст</p>
@@ -28,20 +28,40 @@ const form = document.querySelector('.header-form');
 form.addEventListener('submit', handleSubmit);
 
 const list = document.querySelector('.tasks-list');
+list.addEventListener('click', handkeClick);
+
+const LS_KEY = 'tascs';
+
+let datas = JSON.parse(localStorage.getItem(LS_KEY)) || [];
 
 function handleSubmit(event) {
   event.preventDefault();
-  const title = event.target.elements.taskName.value;
-  const description = event.target.elements.taskDescription.value;
+
+  const title = event.target.elements.taskName.value.trim();
+  const description = event.target.elements.taskDescription.value.trim();
   // console.log(title);
   // console.log(description);
+  if (!title || !description) {
+    return alert('Заповніть усі поля!!!');
+  }
   const message = { title, description, id: nanoid() };
   // console.log(message);
+  datas.push(message);
+  list.insertAdjacentHTML('beforeend', markupItem(message));
 
-  list.insertAdjacentHTML('beforeend', markupItem());
+  event.target.reset();
+  localStorage.setItem(LS_KEY, JSON.stringify(datas));
 }
 
-function markupItem(message) {
+visibleDatas(datas);
+function visibleDatas(datas) {
+  const markupItems = datas.map(markupItem).join('');
+  console.log(markupItems);
+
+  list.insertAdjacentHTML('beforeend', markupItems);
+}
+
+function markupItem({ title, description, id }) {
   return `
   <li class="task-list-item" data-id="${id}">
       <button class="task-list-item-btn">Delete</button>
@@ -50,4 +70,14 @@ function markupItem(message) {
   </li>
   `;
 }
-console.log(markupItem());
+
+function handkeClick(event) {
+  if (event.target.classList.contains('task-list-item-btn')) {
+    console.log('ok');
+    // localStorage.removeItem(event.target);
+    const currentId = event.target.closest('[data-id]').dataset.id;
+    // console.log(event.target.closest('[data-id]').dataset.id);
+    datas = datas.filter(({ id }) => id !== currentId);
+    // презаписати датас,
+  }
+}
